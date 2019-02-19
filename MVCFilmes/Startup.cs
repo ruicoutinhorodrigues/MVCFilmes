c4d48ec3d2023c93eb0c4650d25d389b408e368a
Rui Rodrigues <rui.coutinho.rodrigues@gmail.com>
2019-02-19 09:12:06 +0000

    Init commit

---
+++ MVCFilmes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCFilmes.Startup))]
+namespace MVCFilmes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
